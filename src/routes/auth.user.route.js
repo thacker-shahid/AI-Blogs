51f02b2f5b0354b554a7c3c0869b2c0018fd8785
thacker-shahid <tauheedshahid14@gmail.com>
2024-10-26 07:22:57 +0530

    added delete comments and get all comments route

--- src/routes/auth.user.route.js
+++ src/routes/auth.user.route.js
@@ -5,23 +5,22 @@ const generateToken = require('../middleware/generateToken');
 // const session = require('express-session');
 
 // Register a new user
-router.post('/register', async(req, res)=>{
-    try{
-        const {username, password, email} = req.body;
-        const user = new User({username, password, email});
+router.post('/register', async (req, res) => {
+    try {
+        const { username, password, email } = req.body;
+        const user = new User({ username, password, email });
 
-        if(user){
-                await user.save();
-                res.status(201).send({ message: 'User registered successfully!', user: user });
-        } else{
+        if (user) {
+            await user.save();
+            res.status(201).send({ message: 'User registered successfully!', user: user });
+        } else {
             res.status(400).send({ message: 'Invalid user data!' });
         }
-    } catch(error){
+    } catch (error) {
         console.error('Error registering new user', error);
         res.status(500).send({ message: 'Cannot register new user due to some errors!' });
     }
 });
-
 
 // Login a user
 router.post('/login', async (req, res) => {
@@ -29,19 +28,19 @@ router.post('/login', async (req, res) => {
         const { email, password } = req.body;
 
         const user = await User.findOne({ email });
-        
-        if(!user) 
-            return res.status(404).send({ message: 'User not found!'});
+
+        if (!user)
+            return res.status(404).send({ message: 'User not found!' });
 
         const isMatch = await user.comparePassword(password);
-        
-        if(!isMatch) 
+
+        if (!isMatch)
             return res.status(401).send({ message: 'Invalid username or password!' });
-        
+
 
         // Generate and send JWT token
         const token = await generateToken(user._id);
-        
+
         res.cookie('token', token, {
             httpOnly: true,
             secure: true,
@@ -51,54 +50,56 @@ router.post('/login', async (req, res) => {
         // console.log('Cookie set: ', token);
         // localStorage.setItem('token', token)
 
-        res.send({ message: 'Logged in successfully!', token, user: {
-            _id: user._id,
-            username: user.username,
-            email: user.email,
-            role: user.role,
-        }});
+        res.send({
+            message: 'Logged in successfully!', token, user: {
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+                role: user.role,
+            }
+        });
 
-    } catch(error){
+    } catch (error) {
         console.error('Error logging in user', error);
         res.status(500).send({ message: 'Cannot login user due to some errors!' });
     }
 });
 
 // Logout a user
-router.post('/logout', async (req, res)=> {
+router.post('/logout', async (req, res) => {
     try {
         res.clearCookie('token');
         res.status(200).send({ message: 'Logged out successfully!' });
-    } catch(error){
+    } catch (error) {
         console.error('Error logging out user', error);
         res.status(500).send({ message: 'Cannot logout user due to some errors!' });
     }
 });
 
 // Get all users
-router.get('/users', async (req, res)=> {  
+router.get('/users', async (req, res) => {
     try {
-    
-        const users = await User.find({}, 'id email role');
-        res.status(200).send({message: "Users found successfully!", users });
 
-    } catch(error){
+        const users = await User.find({}, 'id email role');
+        res.status(200).send({ message: "Users found successfully!", users });
+
+    } catch (error) {
         console.error('Error getting users', error);
         res.status(500).send({ message: 'Cannot get users due to some errors!' });
     }
 });
 
 // Delete a user
-router.delete('/users/:id', async (req, res)=> {
-    try{
+router.delete('/users/:id', async (req, res) => {
+    try {
         const id = req.params.id;
         const userToDelete = await User.findByIdAndDelete(id);
-        if(!userToDelete){
-            res.status(404).send({ message: "No user found with given Id"})
+        if (!userToDelete) {
+            res.status(404).send({ message: "No user found with given Id" })
         }
-        res.status(200).send({ message: "User with given Id deleted successfully!"})
+        res.status(200).send({ message: "User with given Id deleted successfully!" })
 
-    } catch(error){
+    } catch (error) {
         console.error('Error deleting user', error);
         res.status(500).send({ message: 'Cannot delete user due to some errors!' });
     }
@@ -110,12 +111,12 @@ router.put('/users/:id', async (req, res) => {
         const userId = req.params.id;
         const updatedRole = req.body.role;
         const user = await User.findByIdAndUpdate(userId, { role: updatedRole }, { new: true });
-        if(!user){
-            res.status(404).send({ message: "No user found with given Id"})
+        if (!user) {
+            res.status(404).send({ message: "No user found with given Id" })
         }
         res.status(200).send({ message: "User role updated successfully!", user });
 
-    } catch(error) {
+    } catch (error) {
         console.error('Error updating user role', error);
         res.status(500).send({ message: 'Cannot update user role due to some errors!' });
     }
--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -7,32 +7,67 @@ const Comment = require('../models/comments.model')
 router.post('/post-comment', async (req, res) => {
     try {
         const newComment = new Comment(req.body);
-        if(newComment){
+        if (newComment) {
             await newComment.save();
-            res.status(200).send({ 
-                message: "New Comment added successfully!", 
-                comment: newComment 
+            res.status(200).send({
+                message: "New Comment added successfully!",
+                comment: newComment
             });
         }
 
     } catch (error) {
         console.error("Some error in creating comment", error);
-        res.status(500).send({Message: "Cannot create comment due to some error!"})
+        res.status(500).send({ Message: "Cannot create comment due to some error!" })
     }
 })
 
 //  Get all comments count
-router.get('/total-comments', async (req, res)=> {
+router.get('/total-comments', async (req, res) => {
     try {
         const totalComments = await Comment.countDocuments({});
-        if(totalComments){
+        if (totalComments) {
             res.status(200).send({ commentsCount: totalComments });
-        } else{
-            res.status(404).send({message: "No comments found!"})
+        } else {
+            res.status(404).send({ message: "No comments found!" })
         }
-    } catch(error){
+    } catch (error) {
         console.error("Some error in getting total comments", error);
-        res.status(500).send({Message: "Cannot get total comments due to some error!"})
+        res.status(500).send({ Message: "Cannot get total comments due to some error!" })
+    }
+})
+
+// Get all comments 
+router.get('/all-comments', async (req, res) => {
+    try {
+        const allComments = await Comment.find();
+        if (allComments) {
+            res.status(200).send({ allComments: allComments });
+        } else {
+            res.status(404).send({ message: "No comments found!" })
+        }
+    } catch (error) {
+        console.error("Some error in getting comments", error);
+        res.status(500).send({ Message: "Cannot get comments due to some error!" })
+    }
+})
+
+// Delete a comment
+router.delete('/delete-comment/:id', async (req, res) => {
+    try {
+        const commentId = req.params.id;
+        const commentToDelete = await Comment.findByIdAndDelete(commentId);
+
+        if (!commentToDelete) {
+            res.status(404).send({ message: "No comment found with given Id" })
+        } else {
+            res.status(200).send({
+                message: "Comment with given Id deleted successfully!",
+                comment: commentToDelete
+            })
+        }
+    } catch (error) {
+        console.error("Some error in deleting a comment", error);
+        res.status(500).send({ Message: "Cannot delete a comment due to some error!" })
     }
 })
 
